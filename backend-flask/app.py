9349bcd39847cfc0a34fc944ca24b0c7e30cb25e
Ryan Richard <tank1332@me.com>
2023-02-27 23:12:33 +0000

    Got rid of extra opentelemetry import statement.

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -21,7 +21,6 @@ from opentelemetry.instrumentation.requests import RequestsInstrumentor
 from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
-from opentelemetry import trace
 
 # Initialize tracing and an exporter that can send data to Honeycomb
 provider = TracerProvider()

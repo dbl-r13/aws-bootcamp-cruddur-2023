562abe2f6282fbabf496b26705b8186a42b1be58
Ryan Richard <tank1332@me.com>
2023-03-04 05:40:17 +0000

    Added the import statements for the Cloudwatch Logs. Also, passed in the logger information to the proper calls so they could feed into the proper services.

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -26,6 +26,11 @@ from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from aws_xray_sdk.core import xray_recorder
 from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
 
+# Cloudwatch Logs
+import watchtower
+import logging
+from time import strftime
+
 
 
 # Initialize tracing and an exporter that can send data to Honeycomb
@@ -37,6 +42,16 @@ tracer = trace.get_tracer('__name__')
 
 
 app = Flask(__name__)
+
+# Configuring Logger to Use CloudWatch
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.DEBUG)
+console_handler = logging.StreamHandler()
+cw_handler = watchtower.CloudWatchLogHandler(log_group='cruddur')
+LOGGER.addHandler(console_handler)
+LOGGER.addHandler(cw_handler)
+LOGGER.info("some messages are not worth it.")
+LOGGER.info('Test log')
 
 # AWS X-ray
 xray_url = os.getenv("AWS_XRAY_URL")
@@ -57,6 +72,12 @@ cors = CORS(
   allow_headers="content-type,if-modified-since",
   methods="OPTIONS,GET,HEAD,POST"
 )
+
+@app.after_request
+def after_request(response):
+    timestamp = strftime('[%Y-%b-%d %H:%M]')
+    LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
+    return response
 
 @app.route("/api/message_groups", methods=['GET'])
 def data_message_groups():
@@ -95,12 +116,12 @@ def data_create_message():
 
 @app.route("/api/activities/home", methods=['GET'])
 def data_home():
-  data = HomeActivities.run()
+  data = HomeActivities.run(logger=LOGGER)
   return data, 200
 
 @app.route("/api/activities/notifications", methods=['GET'])
 def data_notifications():
-  data = NotificationsActivities.run()
+  data = NotificationsActivities.run(logger=LOGGER)
   return data, 200
 
 @app.route("/api/activities/@<string:handle>", methods=['GET'])

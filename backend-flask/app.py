91bce8df5e47f1f92e7d562d1f68495f804166fe
Ryan Richard <tank1332@me.com>
2023-03-05 20:32:00 +0000

    Added Rollbar information for import, access token and for test endpoint.

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -31,6 +31,11 @@ import watchtower
 import logging
 from time import strftime
 
+# Rollbar
+import rollbar
+import rollbar.contrib.flask
+from flask import got_request_exception
+
 
 
 # Initialize tracing and an exporter that can send data to Honeycomb
@@ -73,6 +78,24 @@ cors = CORS(
   methods="OPTIONS,GET,HEAD,POST"
 )
 
+#Rollbar
+rollbar_access_token="${ROLLBAR_ACCESS_TOKEN}"
+@app.before_first_request
+def init_rollbar():
+    """init rollbar module"""
+    rollbar.init(
+        # access token
+        rollbar_access_token,
+        # environment name
+        'production',
+        # server root directory, makes tracebacks prettier
+        root=os.path.dirname(os.path.realpath(__file__)),
+        # flask already sets up logging
+        allow_logging_basic_config=False)
+
+    # send exceptions from `app` to rollbar, using flask's signal system.
+    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
+
 @app.after_request
 def after_request(response):
     timestamp = strftime('[%Y-%b-%d %H:%M]')
@@ -99,6 +122,11 @@ def data_messages(handle):
   else:
     return model['data'], 200
   return
+
+@app.route('/rollbar/test')
+def rollbar_test():
+    rollbar.report_message('Hello World!', 'warning')
+    return "Hello World!"
 
 @app.route("/api/messages", methods=['POST','OPTIONS'])
 @cross_origin()
